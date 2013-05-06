16b6c167c5211204cbcc22a54a2928dc41cdf43d
Emanuel Evans <emanuel.evans@gmail.com>
2013-05-06 01:39:50 -0700

    Fix new/edit time inputs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
   def format_date(date)
-    date.strftime('%m/%d/%Y')
+    date.strftime('%m/%d/%Y') unless date.nil?
   end
 
   def format_time(time)
-    time.strftime('%I:%M %p')
+    time.strftime('%I:%M %p') unless time.nil?
+  end
+
+  def format_rfc_time(time)
+    time.strftime('%H:%M:%S') unless time.nil?
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,11 +6,26 @@ describe ApplicationHelper do
       expect(format_date(Date.parse('2013-05-08'))).to eql('05/08/2013')
       expect(format_date(Date.parse('08/10/2012'))).to eql('10/08/2012')
     end
+    it "should return nil for empty times" do
+      expect(format_date(nil)).to be_nil
+    end
   end
   describe "format_time" do
-    it "should nicely format times"do
+    it "should nicely format times" do
       expect(format_time(Time.parse('16:45'))).to eql('04:45 PM')
       expect(format_time(Time.parse('3:18 PM'))).to eql('03:18 PM')
+    end
+    it "should return nil for empty times" do
+      expect(format_time(nil)).to be_nil
+    end
+  end
+  describe "format_rfc_time" do
+    it "should format dates according to RFC 3999" do
+      expect(format_rfc_time(Time.parse('3:45 PM'))).to eql('15:45:00')
+      expect(format_rfc_time(Time.parse('3 AM'))).to eql('03:00:00')
+    end
+    it "should return nil for empty times" do
+      expect(format_rfc_time(nil)).to be_nil
     end
   end
 end
