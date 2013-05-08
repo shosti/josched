d38eacda1e631b9b86004e59375bab7be02c9b62
Emanuel Evans <emanuel.evans@gmail.com>
2013-05-07 18:31:06 -0700

    Add sleep pseudo-event

--- app/models/pseudo_event.rb
+++ app/models/pseudo_event.rb
@@ -14,8 +14,20 @@ class PseudoEvent < Event
   end
 
   def self.free_time(start_min, end_min)
-    e = PseudoEvent.new(name: 'Free',
-                        start_min: start_min,
-                        end_min: end_min)
+    PseudoEvent.new(name: 'Free',
+                    start_min: start_min,
+                    end_min: end_min)
+  end
+
+  def self.sleep_until(end_min)
+    PseudoEvent.new(name: 'Sleep',
+                    start_min: 0,
+                    end_min: end_min)
+  end
+
+  def self.sleep_after(start_min)
+    PseudoEvent.new(name: 'Sleep',
+                    start_min: start_min,
+                    end_min: 24 * 60)
   end
 end
--- spec/models/pseudo_event_spec.rb
+++ spec/models/pseudo_event_spec.rb
@@ -21,4 +21,15 @@ describe PseudoEvent do
     free_time.start_time.should eql '8:00 AM'
     free_time.end_time.should eql '9:30 PM'
   end
+
+  it "can be a sleep" do
+    sleep_until = PseudoEvent.sleep_until(Event.time_to_min('8:00 AM'))
+    sleep_after = PseudoEvent.sleep_after(Event.time_to_min('11:00 PM'))
+
+    sleep_until.name.should eql 'Sleep'
+    sleep_until.start_time.should eql '4:00 AM'
+    sleep_until.end_time.should eql '8:00 AM'
+    sleep_after.start_time.should eql '11:00 PM'
+    sleep_after.end_time.should eql '4:00 AM'
+  end
 end
